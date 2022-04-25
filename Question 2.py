44a88f57a9af7a9d211489cd54acfbea8afba2b4
Rudransh21102068 <104332012+Rudransh21102068@users.noreply.github.com>
2022-04-25 22:46:36 +0530

    Add files via upload

---
+++ Question 2.py
@@ -0,0 +1,21 @@
+# Initialize the constants
+
+TAX_RATE= 0.20
+
+STANDARD_DEDUCTION = 10000
+
+DEPENDENT_DEDUCTION = 3000
+
+# Request the inputs
+
+grossIncome = float(input("Enter the gross income: " ))
+
+numDependents = int(input("Enter the number of dependents: "))
+
+# Compute the income tax
+
+Taxable_income = Gross_Income - Standard__deduction - (Dependent_deduction * No. of dependents)
+
+Tax = Taxable Income * Tax Rate
+
+# Display the income tax print("The income tax is $" + str(income Tax))
