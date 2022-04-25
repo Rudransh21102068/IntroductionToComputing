81ad7fbeae97da2da4b1b25d3a47fb8372466884
Rudransh21102068 <104332012+Rudransh21102068@users.noreply.github.com>
2022-04-25 07:48:46 +0530

    Add files via upload

---
+++ QUESTION 5 (a).py
@@ -0,0 +1,6 @@
+color = ['Red', 'Green', 'White', 'Black', 'Pink', 'Yellow'] 
+print("The list is: ", color)  
+  
+  
+color.remove('Black')  
+print("After removing element: ",color)  
---
+++ Question 4.py
@@ -0,0 +1,16 @@
+m1 = int(input("Marks of 1st student : "))
+
+m2 = int(input("Marks of 2nd student : "))
+
+m3 = int(input("Marks of 3rd student : "))
+
+m4 = int(input("Marks of 4th student : "))
+
+m5 = int(input("Marks of 5th student : "))
+
+
+SortedMarks = [m1, m2, m3, m4, m5]
+
+SortedMarks.sort()
+
+print(SortedMarks)
