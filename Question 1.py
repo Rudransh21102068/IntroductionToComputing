72d8d464b178caa667a06b8edec680fbea0d9ede
Rudransh21102068 <104332012+Rudransh21102068@users.noreply.github.com>
2022-04-25 06:38:13 +0530

    Add files via upload

---
+++ Question 1.py
@@ -0,0 +1,11 @@
+a = input('Enter 1st Number : ')
+b = input('Enter 2nd Number : ')
+c = input('Enter 3rd Number : ')
+
+a = int(a)
+b = int(b)
+c = int(c)
+
+sum = a + b + c
+avg = sum / 3
+print('Average of all 3 Numbers is  {}'.format(avg))
