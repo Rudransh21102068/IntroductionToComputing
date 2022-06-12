f6fe782a028870cb9df7396bd87f1360866632cf
Rudransh21102068 <104332012+Rudransh21102068@users.noreply.github.com>
2022-06-12 16:58:08 +0530

    Add files via upload

---
+++ QUESTION 5 (b).py
@@ -0,0 +1,4 @@
+List_of_colors = ['Red', 'Green', 'White', 'Black', 'Pink', 'Yellow']
+print("List of colors before replacing:",List_of_colors)
+List_of_colors[3:5]= ['Purple']
+print("your list after replacing black and pink with purple:", List_of_colors)
